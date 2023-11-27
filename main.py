a4a5dffb4c5aa2a443df1623f645c197b1dc6bc2
ge83vad <hoomanasgari95@gmail.com>
2023-11-27 11:49:19 +0100

    This code works perfect up to compare_models. It trains LeNet over MNIST datset. The reconstructed model is identical with the trained one.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import traceback
 import copy
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, models, transforms
 from torch.utils.data import DataLoader, Subset
-import torch.nn.functional as F
 
 class LeNet(nn.Module):
     def __init__(self):
@@ -36,35 +34,39 @@ def load_mnist(root='./data',
     return train_dataset, test_dataset
 
 
+
 def train_model(model, criterion, optimizer, train_loader, num_epochs):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = model.to(device)
+    model.to(device)
 
-    # Store the pre-training state
-    pre_training_state = {name: param.clone() for name, param in model.named_parameters()}
+    # Store the complete pre-training state of the model
+    pre_training_state = copy.deepcopy(model.state_dict())
 
     # Training Loop
+
     for epoch in range(num_epochs):
-        model.train()
         for inputs, labels in train_loader:
             inputs, labels = inputs.to(device), labels.to(device)
-
-            # Forward pass
+            model.train()
+            optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, labels)
-
-            # Backward pass and optimization
-            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
         print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}")
 
-    # Calculate the parameter differences
-    model_difference = {name: model.state_dict()[name] - pre_training_state[name] for name in model.state_dict()}
+    # Calculate the parameter differences post-training
+    model_difference = {name: model.state_dict()[name] - pre_training_state[name]
+                        for name in model.state_dict()}
+
+    # Copy BatchNorm running mean and variance directly
+    for name, module in model.named_modules():
+        if isinstance(module, nn.BatchNorm2d):
+            model_difference[f"{name}.running_mean"] = model.state_dict()[f"{name}.running_mean"].clone()
+            model_difference[f"{name}.running_var"] = model.state_dict()[f"{name}.running_var"].clone()
 
     return model, model_difference
-
 
 
 def reconstruct_model(original_model, model_difference):
@@ -78,7 +80,28 @@ def reconstruct_model(original_model, model_difference):
                 # Update the parameter with the difference
                 param.data.add_(model_difference[name])
 
+        # Update the running mean and variance for BatchNorm layers
+        for name, module in reconstructed_model.named_modules():
+            if isinstance(module, nn.BatchNorm2d):
+                if f"{name}.running_mean" in model_difference:
+                    module.running_mean.copy_(model_difference[f"{name}.running_mean"])
+                if f"{name}.running_var" in model_difference:
+                    module.running_var.copy_(model_difference[f"{name}.running_var"])
+
     return reconstructed_model
+def load_cifar10(root='./data', fraction=0.02, transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])):
+    train_dataset = datasets.CIFAR10(root=root, train=True, download=True, transform=transform)
+    test_dataset = datasets.CIFAR10(root=root, train=False, download=True, transform=transform)
+
+    # Reduce dataset size
+    indices = torch.randperm(len(train_dataset)).tolist()
+    reduced_size = int(fraction * len(indices))
+    train_dataset_reduced = Subset(train_dataset, indices[:reduced_size])
+
+    return train_dataset_reduced, test_dataset
+
 
 
 # Function to evaluate the accuracy of a model
@@ -97,34 +120,17 @@ def evaluate_accuracy(model, data_loader):
     accuracy = 100 * correct / total
     return accuracy
 
-def compare_models(trained_model, reconstructed_model):
-    for (name1, param1), (name2, param2) in zip(trained_model.named_parameters(), reconstructed_model.named_parameters()):
-        if param1.data.nelement() == 0:  # Skip parameters with no data
-            continue
-
-        # Calculate the relative difference
-        relative_diff = torch.abs(np.subtract(param1, param2)) / torch.abs(param1)
-        counter = 0
-        # Check if any element in the difference tensor exceeds the threshold (1%)
-        if torch.any(relative_diff > 0.01):
-            counter += 1
-            print(torch.norm(param1 - param2))
-            print(f"Parameter: {name1}")
-            print(f"Trained Model: {param1.data}")
-            print(f"Reconstructed Model: {param2.data}\n")
-    print(counter)
 
 
 def main():
     try:
         num_epochs = 5
-        # Step 1: Load and Prepare the CIFAR10 Dataset or MNIST
+        # Step 1: Load and Prepare the CIFAR10 Dataset
         transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.5,), (0.5,))  # Normalize for 1-channel grayscale images
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
-
-        train_set, test_set = load_mnist(root='./data', transform=transform)
+        train_set, test_set = load_cifar10(root='./data', transform=transform)
 
         indices = torch.randperm(len(train_set)).tolist()
         train_set_reduced = Subset(train_set, indices[:int(0.1 * len(indices))])
@@ -132,9 +138,8 @@ def main():
         train_loader = DataLoader(train_set_reduced, batch_size=64, shuffle=True, num_workers=2)
 
         # Step 2: Define the ResNet18 Model or LeNet
-        #model = models.resnet18(pretrained=False)
-        #model.fc = nn.Linear(model.fc.in_features, 10)  # CIFAR10 has 10 classes
-        model = LeNet()
+        model = models.resnet18(pretrained=False)
+        model.fc = nn.Linear(model.fc.in_features, 10)  # CIFAR10 has 10 classes
         model_copy = copy.deepcopy(model)
 
         # Step 3: Training Loop
@@ -147,13 +152,6 @@ def main():
         reconstructed_model = reconstruct_model(model_copy, model_difference)
 
         # Step 6: Evaluate the Model
-        # Here you would include code to evaluate the accuracy of trained_model and reconstructed_model
-        # Evaluate accuracies
-        # Step 1: Load the CIFAR10 Test Dataset
-        #test_transform = transforms.Compose([transforms.ToTensor(),
-                                             #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-        #test_set = datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
         test_loader = DataLoader(test_set, batch_size=64, shuffle=False, num_workers=2)
 
         trained_model_accuracy = evaluate_accuracy(trained_model, test_loader)
@@ -162,10 +160,8 @@ def main():
         print(f"Accuracy of Trained Model: {trained_model_accuracy:.2f}%")
         print(f"Accuracy of Reconstructed Model: {reconstructed_model_accuracy:.2f}%")
 
-        # Call this function in your main after reconstructing the model
-        compare_models(trained_model, reconstructed_model)
     except Exception as e:
-        print(f"An error occured : {e}")
+        print(f"An error occured:{e}")
         traceback.print_exc()
 
 if __name__ == "__main__":
